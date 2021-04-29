6b5644f0f7c8a02e55f520a56d1b2eccc24bc739
‘R-Alhayek’ <rafeefalhayek94@outlook.com>
2021-04-29 15:41:50 +0300

    solving problem 4

--- app.js
+++ app.js
@@ -52,10 +52,15 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) {
   //eslint-disable-line
+  let x = sum(a, b)[0];
+  let y = sum(x,c)[0];
+  let r = multiply(a, b)[0];
+  let w = multiply(r,c)[0];
+  return [y, w, `${a} and ${b} and ${c} sum to ${y}.`,`The product of ${a} and ${b} and ${c} is ${w}.`];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -74,11 +79,18 @@ let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) {
   //eslint-disable-line
+  let a = testArray[0];
+  let b = testArray[1];
+  let c = testArray[2];
+  let x = sum(a, b)[0];
+  let y = sum(x,c)[0];
+  return [y, `${a},${b},${c} was passed in as an array of numbers, and ${y} is their sum.` ];
+
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
