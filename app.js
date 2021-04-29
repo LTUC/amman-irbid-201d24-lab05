25f138c6626a2a800686571ce119164e091c3846
KZTahat <kztahat96@gmail.com>
2021-04-29 14:21:47 +0300

    solving problem 4

--- app.js
+++ app.js
@@ -83,20 +83,27 @@ that EXACTLY follows this example and uses the values that were input into the f
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
 IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() 
-function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+function that you've already created. You're going to have to be resourceful to figure out how to do this. 
+However, you may continue to use the + operator for string concatenation.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, 
+uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) {
   //eslint-disable-line
+  let sum =0;
+  for(let i =0;i<sumArr.length;i++){
+    sum=sum+sumArr[i];
+  }
+  return [sum,`${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sum} is their sum.`];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
