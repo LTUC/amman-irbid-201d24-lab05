b6521a950ebb07f179b8a04babc13ade3fa2ace8
SJAljawhary <sndjehad@gmail.com>
2021-05-04 22:58:07 +0300

    reset lab05

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) {
-  let sum = a+b; 
-  return[sum ,`The sum of ${a} and ${b} is ${sum}.` ]
   //eslint-disable-line
 }
 // Here is the test for sum(); uncomment it to run it
@@ -29,12 +27,10 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) {
   //eslint-disable-line
-  let multi = a*b;
-  return[multi ,`The product of ${a} and ${b} is ${multi}.`]
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -50,27 +46,12 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-// function sumAndMultiply(a, b, c) {
-//   
-
 function sumAndMultiply(a, b, c) {
-  let totalsum=sum(sum(a,b)[0],c)[0];
-
-  let totalmulti =multiply(multiply(a,b)[0],c)[0];
-
-//  console.log(totalsum);
-//  console.log(totalmulti);
-
- return[totalsum,totalmulti,`${a} and ${b} and ${c} sum to ${totalsum}.`,`The product of ${a} and ${b} and ${c} is ${totalmulti}.`]
- 
-   //eslint-disable-line
- }
-
   //eslint-disable-line
-// t
-// Here is the test for sumAndMultiply(); uncomment it to run it
+}
 
-testSumAndMultiply(4,7,5);
+// Here is the test for sumAndMultiply(); uncomment it to run it
+// testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -85,18 +66,11 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-// let testArray = [2, 3, 4]; //eslint-disable-line
+let testArray = [2, 3, 4]; //eslint-disable-line
 
-// function sumArray(sumArr) {
-
-
-//   let sumArr = sum((a,b)[0],c)[0];
-
-//   return[arraySum,` ${a},${b},${c} was passed in as an array of numbers, and ${arraySum} is their sum.` ]
-
-
+function sumArray(sumArr) {
   //eslint-disable-line
-////////////
+}
 
 // Here is the test for sumArray(); uncomment it to run it
 
